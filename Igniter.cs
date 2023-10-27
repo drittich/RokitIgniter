61e60d6e19cac4c5280af374b861f4c6bae9ef35
D'Arcy Rittich <drittich@gmail.com>
2023-10-27 11:52:34 -0400

    switch to tray-only interface, port to .NET 8

---
+++ Igniter.cs
@@ -0,0 +1,62 @@
+﻿using System.Media;
+
+namespace RokitIgniter
+{
+	public class Igniter : IDisposable
+	{
+		CancellationTokenSource? _cts;
+		Task? _timerTask;
+		readonly TimeSpan interval = TimeSpan.FromMinutes(25);
+
+		public Igniter()
+		{
+			_timerTask = DoStart(interval);
+		}
+
+		async Task DoStart(TimeSpan interval)
+		{
+			_cts = new();
+			try
+			{
+				using var timer = new PeriodicTimer(interval);
+				do
+				{
+					PlaySound();
+				}
+				while (await timer.WaitForNextTickAsync(_cts.Token));
+			}
+			catch (OperationCanceledException)
+			{
+			}
+		}
+
+		void PlaySound()
+		{
+			using (SoundPlayer player = new SoundPlayer(Path.Combine(Environment.CurrentDirectory, "10Hz.wav")))
+				player.Play();
+		}
+
+		public async Task IgniteAsync()
+		{
+			if (_cts is not null)
+			{
+				_cts.Cancel();
+				await _timerTask!;
+				_cts.Dispose();
+			}
+
+			_timerTask = DoStart(interval);
+		}
+
+		public void Dispose()
+		{
+			if (_cts is not null)
+			{
+				_cts.Cancel();
+				_timerTask!.Wait();
+				_cts.Dispose();
+			}
+			GC.SuppressFinalize(this);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,63 @@ namespace RokitIgniter
 		{
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
-			Application.Run(new Form1());
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			// we don't need a form, so we can just run the ApplicationContext directly
+			Application.Run(new MyCustomApplicationContext());
+		}
+
+		public class MyCustomApplicationContext : ApplicationContext
+		{
+			private NotifyIcon notifyIcon;
+			private Igniter igniter;
+
+			public MyCustomApplicationContext()
+			{
+				notifyIcon = new();
+				igniter = new();
+				SetupNotifyIcon();
+			}
+
+			private void SetupNotifyIcon()
+			{
+				notifyIcon!.Text = "Rokit Igniter";
+				notifyIcon.Icon = new("RokitIgniter.ico");
+				notifyIcon.ContextMenuStrip = new();
+				notifyIcon.ContextMenuStrip.Items.Add("Ignite", null, async (s, e) => await Ignite_ClickAsync(s, e));
+				notifyIcon.ContextMenuStrip.Items.Add("About", null, ShowAboutMessageBox_Click);
+				notifyIcon.ContextMenuStrip.Items.Add("Exit", null, ExitApplication_Click);
+
+				notifyIcon!.Click += async (s, e) =>
+				{
+					if (e is MouseEventArgs me && me.Button == MouseButtons.Right)
+					{
+						return;
+					}
+
+					await igniter.IgniteAsync();
+				};
+
+				notifyIcon.Visible = true;
+			}
+
+			private void ExitApplication_Click(object? sender, EventArgs e)
+			{
+				Environment.Exit(0);
+			}
+
+			async Task Ignite_ClickAsync(object? sender, EventArgs? e)
+			{
+				await igniter!.IgniteAsync();
+			}
+
+			void ShowAboutMessageBox_Click(object? sender, EventArgs? e)
+			{
+				var packageVersion = typeof(Program).Assembly.GetName().Version;
+				MessageBox.Show($"RokitIgniter v{packageVersion}", "RokitIgniter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
 		}
 	}
 }
